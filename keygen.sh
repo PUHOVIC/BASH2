23ce5eb2f361d771bd81bb51e4e168a57cddd826
PUHOVIC <alh36009@gmail.com>
2025-01-21 11:14:50 -0500

    added forest

--- keygen.sh
+++ keygen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir key
+mkdir -p key 
+for i in {1..300}; do
+    touch "key/file$i"
+done
 
-touch key/file{1..300}
-
-for i in {1..60}
-do
-    echo $((RANDOM%10 + 1)) > key/part${i}.key
+for i in {1..60}; do
+    echo $((RANDOM % 10 + 1)) > "key/part${i}.key"
 done
 
 echo "Generation completed"
